0e4f418da3c629d2af1853630214c7a95ae33699
alliewww <aa2993.c@gmail.com>
2025-09-27 14:51:28 +0900

    Nginx設定の変更

--- webapp/frontend/next.config.ts
+++ webapp/frontend/next.config.ts
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  compress: false, // 由 Nginx 壓縮
+  poweredByHeader: false,
+  modularizeImports: {
+    "@mui/material": { transform: "@mui/material/{{member}}" },
+    "@mui/icons-material": { transform: "@mui/icons-material/{{member}}" },
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        ],
+      },
+    ];
+  },
 };
 export default nextConfig;
